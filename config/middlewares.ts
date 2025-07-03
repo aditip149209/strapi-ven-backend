b44be252b29b4102fc97ebcb0392c722afcd5078
Aditi Pandey <aditip149209@gmail.com>
2025-07-03 20:56:37 +0530

    changes cors in config

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -10,3 +10,22 @@ export default [
   'strapi::favicon',
   'strapi::public',
 ];
+
+module.exports = [
+  'strapi::errors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['https://strapi-ven-backend.onrender.com'], // Or ['*'] for testing only
+      credentials: true,
+    },
+  },
+  'strapi::security',
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
