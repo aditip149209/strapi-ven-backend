c96a80fee5180b56c0d9d9116594acc127232115
Aditi Pandey <aditip149209@gmail.com>
2025-07-03 22:14:26 +0530

    cors7

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,7 +1,23 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: '*', // or your frontend URL for production
+      headers: '*',
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    },
+  },
+  // Custom middleware for static file CORS
+  async (ctx, next) => {
+    await next();
+    if (ctx.path.startsWith('/uploads/')) {
+      ctx.set('Access-Control-Allow-Origin', '*'); // or your frontend URL
+      ctx.set('Access-Control-Allow-Methods', 'GET,HEAD,PUT,POST,DELETE,OPTIONS');
+      ctx.set('Access-Control-Allow-Headers', '*');
+    }
+  },
   'strapi::security',
   'strapi::poweredBy',
   'strapi::query',
