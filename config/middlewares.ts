4f8e8db6d6bb999e351b77811f8b6997299be532
Aditi Pandey <aditip149209@gmail.com>
2025-07-04 12:34:17 +0530

    changed formsubmission url from localhost to render url

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,23 +1,7 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  {
-    name: 'strapi::cors',
-    config: {
-      origin: '*', // or your frontend URL for production
-      headers: '*',
-      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    },
-  },
-  // Custom middleware for static file CORS
-  async (ctx, next) => {
-    await next();
-    if (ctx.path.startsWith('/uploads/')) {
-      ctx.set('Access-Control-Allow-Origin', '*'); // or your frontend URL
-      ctx.set('Access-Control-Allow-Methods', 'GET,HEAD,PUT,POST,DELETE,OPTIONS');
-      ctx.set('Access-Control-Allow-Headers', '*');
-    }
-  },
+  'strapi::cors',
   'strapi::security',
   'strapi::poweredBy',
   'strapi::query',
